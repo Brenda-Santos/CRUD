df68c7d1c71b2e3325db36b175d83f04b9107721
Brenda Santos <brenda2santos@outlook.com>
2020-05-18 20:58:34 -0300

    validations

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,23 +4,29 @@
 
     <h1 class="text-center mt-5">Cadastrar</h1>
 
-    
-
     <div class="col-8 m-auto">
+         
+        @if(isset($erros) && count($errors)>0)
+            <div class='text-center mt-4 mb-4 p-2 danger'>
+                @foreach($errors->all() as $erro)
+                    {{$erro}}
+                @endforeach
+            </div>
+        @endif
     
-    <form name='formCad' id='formCad' method='post' action='{{url('books')}}'>
-    @csrf
-    <input class='form-control mt-5' type='text' name='title' id='title' placeholder='Título'>
-    <select class='form-control mt-4' name='id_user' id='id_user'>
-    <option value=''>Autor</option>
-        @foreach($users as $user)
-            <option value='{{$user->id}}'>{{$user->name}}</option>
-        @endforeach
-    </select>
-    <input class='form-control mt-4' type='text' name='pages' id='pages' placeholder='Páginas'>
-    <input class='form-control mt-4' type='text' name='price' id='price' placeholder='Preço'>
-    <input class='btn btn-primary mt-4' type='submit' value='Cadastrar'>
-    </form>
+        <form name='formCad' id='formCad' method='post' action='{{url('books')}}'>
+        @csrf
+        <input class='form-control mt-5' type='text' name='title' id='title' placeholder='Título' required>
+        <select class='form-control mt-4' name='id_user' id='id_user' required>
+            <option value=''>Autor</option>
+            @foreach($users as $user)
+                <option value='{{$user->id}}'>{{$user->name}}</option>
+            @endforeach
+        </select>
+        <input class='form-control mt-4' type='text' name='pages' id='pages' placeholder='Páginas' required>
+        <input class='form-control mt-4' type='text' name='price' id='price' placeholder='Preço' required>
+        <input class='btn btn-primary mt-4' type='submit' value='Cadastrar'>
+        </form>
     
 
     </div>
