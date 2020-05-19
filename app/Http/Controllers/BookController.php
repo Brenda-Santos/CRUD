87250b5521eb0a18b94609a4050305a19e173607
Brenda Santos <brenda2santos@outlook.com>
2020-05-18 22:00:32 -0300

    update button

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -79,7 +79,9 @@ class BookController extends Controller
      */
     public function edit($id)
     {
-        //
+        $book=$this->objBook->find($id);
+        $users=$this->objBook->all();
+        return view('create', compact('book','users'));
     }
 
     /**
@@ -91,7 +93,13 @@ class BookController extends Controller
      */
     public function update(BookRequest $request, $id)
     {
-        //
+        $this->objBook->where(['id'=>$id])->update([
+            'title'=>$request->title,
+            'pages'=>$request->pages,
+            'price'=>$request->price,
+            'id_user'=>$request->id_user
+        ]);
+        return redirect ('books');
     }
 
     /**
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1 class="text-center mt-5">Cadastrar</h1>
+    <h1 class="text-center mt-5">@if(isset($book))Editar @else Cadastrar @endif</h1>
 
     <div class="col-8 m-auto">
          
@@ -13,19 +13,25 @@
                 @endforeach
             </div>
         @endif
-    
-        <form name='formCad' id='formCad' method='post' action='{{url('books')}}'>
+
+        @if(isset($book))
+            <form name='formEdit' id='formEdit' method='post' action='{{url('books/$book->id')}}'>
+                @method('PUT')
+        @else        
+            <form name='formCad' id='formCad' method='post' action='{{url('books')}}'>
+        @endif
+
         @csrf
-        <input class='form-control mt-5' type='text' name='title' id='title' placeholder='Título' required>
+        <input class='form-control mt-5' type='text' name='title' id='title' placeholder='Título' value="{{$book->title ?? ''}}" required >
         <select class='form-control mt-4' name='id_user' id='id_user' required>
-            <option value=''>Autor</option>
+            <option value="{{$book->relUsers->id ?? ''}}">{{$book->relUsers->name ?? 'Autor'}}</option>
             @foreach($users as $user)
                 <option value='{{$user->id}}'>{{$user->name}}</option>
             @endforeach
         </select>
-        <input class='form-control mt-4' type='text' name='pages' id='pages' placeholder='Páginas' required>
-        <input class='form-control mt-4' type='text' name='price' id='price' placeholder='Preço' required>
-        <input class='btn btn-primary mt-4' type='submit' value='Cadastrar'>
+        <input class='form-control mt-4' type='text' name='pages' id='pages' placeholder='Páginas' value="{{$book->pages ?? ''}}" required>
+        <input class='form-control mt-4' type='text' name='price' id='price' placeholder='Preço' value="{{$book->price ?? ''}}" required>
+        <input class='btn btn-primary mt-4' type='submit' value='@if(isset($book))Editar @else Cadastrar @endif'>
         </form>
     
 
