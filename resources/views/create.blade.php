5a740a6f6d5f4ddd45bfe3c8ddcc6b280d1e0161
Brenda Santos <brenda2santos@outlook.com>
2020-05-18 23:53:44 -0300

    upgrade edit

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,30 +9,30 @@
         @if(isset($erros) && count($errors)>0)
             <div class='text-center mt-4 mb-4 p-2 danger'>
                 @foreach($errors->all() as $erro)
-                    {{$erro}}
+                    {{$erro}}<br>
                 @endforeach
             </div>
         @endif
 
         @if(isset($book))
-            <form name='formEdit' id='formEdit' method='post' action='{{url('books/$book->id')}}'>
-                @method('PUT')
+            <form name='formEdit' id='formEdit' method='post' action="{{url("books/$book->id")}}">
+            @method('PUT')
         @else        
-            <form name='formCad' id='formCad' method='post' action='{{url('books')}}'>
+            <form name='formCad' id='formCad' method='post' action="{{url("books")}}">
         @endif
 
-        @csrf
-        <input class='form-control mt-5' type='text' name='title' id='title' placeholder='Título' value="{{$book->title ?? ''}}" required >
-        <select class='form-control mt-4' name='id_user' id='id_user' required>
-            <option value="{{$book->relUsers->id ?? ''}}">{{$book->relUsers->name ?? 'Autor'}}</option>
-            @foreach($users as $user)
-                <option value='{{$user->id}}'>{{$user->name}}</option>
-            @endforeach
-        </select>
-        <input class='form-control mt-4' type='text' name='pages' id='pages' placeholder='Páginas' value="{{$book->pages ?? ''}}" required>
-        <input class='form-control mt-4' type='text' name='price' id='price' placeholder='Preço' value="{{$book->price ?? ''}}" required>
-        <input class='btn btn-primary mt-4' type='submit' value='@if(isset($book))Editar @else Cadastrar @endif'>
-        </form>
+            @csrf
+            <input class='form-control mt-5' type='text' name='title' id='title' placeholder='Título' value="{{$book->title ?? ''}}" required >
+            <select class='form-control mt-4' name='id_user' id='id_user' required>
+                <option value="{{$book->relUsers->id ?? ''}}">{{$book->relUsers->name ?? 'Autor'}}</option>
+                @foreach($users as $user)
+                    <option value='{{$user->id}}'>{{$user->name}}</option>
+                @endforeach
+            </select>
+            <input class='form-control mt-4' type='text' name='pages' id='pages' placeholder='Páginas' value="{{$book->pages ?? ''}}" required>
+            <input class='form-control mt-4' type='text' name='price' id='price' placeholder='Preço' value="{{$book->price ?? ''}}" required>
+            <input class='btn btn-primary mt-4' type='submit' value='@if(isset($book))Editar @else Cadastrar @endif'>
+            </form>
     
 
     </div>
